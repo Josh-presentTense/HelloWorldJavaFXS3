16559e33ec3a372bcbc44ddd96dc6c07981b4f22
Josh-presentTense <200464522@student.georgianc.on.ca>
2021-02-03 17:12:55 -0500

    Added a Label to the view and updated the controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,17 +2,21 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class Controller {
     @FXML
     private Button button;
+
+    @FXML
+    private Label messageLabel;
 
     /**
      * When the button is clicked, display something in the console
      */
     @FXML
     private void buttonPushed(){
-        System.out.println("The button was pushed");
+        messageLabel.setText("Here is a new message");
 
     }
 }
